b18ab37c7efc908a40f694f5d76ecb2922a31e96
Gerald Bauer <gerald.bauer@gmail.com>
2013-07-03 19:23:14 +0200

    polish widget

--- js/jquery.football.js
+++ js/jquery.football.js
@@ -1,24 +1,63 @@
 
-var sportdb_widget_new = function( widget_id, opts ) {
-  
-  // todo: use options
-  //   make api_path_prefix into an option
-  //
-  // that is, use defaults
-  
-  var _api_path_prefix = '';
-  var _$widget;
-  var _render_round;   // compiled underscore template - nb: it's just a js function
 
-  var _settings;
+////////////////////////////////////
+// football.dp api wrapper
+
+var sportdb_api_new = function( opts )
+{
   var _defaults = {
                 apiPathPrefix: 'http://footballdb.herokuapp.com/api/v1',
-                tplId: null
               };
+  var _settings;
 
   function _debug( msg ) {
       if( window.console && window.console.log ) {
-        window.console.log( "[debug] "+msg );
+        window.console.log( '[debug] '+msg );
+      }
+    }
+
+  function _init( opts )
+  {
+    _settings = _.extend( {}, _defaults, opts );
+    
+    _debug( 'apiPathPrefix: ' + _settings.apiPathPrefix );
+  }
+
+  function _fetch_round( event_key, round_pos, onsuccess )
+  {
+    var api_url = _settings.apiPathPrefix + '/event/' + event_key + '/round/' + round_pos + '?callback=?'; 
+    $.getJSON( api_url, onsuccess );
+  }
+
+  // call "c'tor/constructor"
+  _init( opts );
+
+  // return/export public api
+  return {
+     fetch_round: _fetch_round
+  }
+} // fn sportdb_api_new
+
+
+
+
+var sportdb_widget_new = function( id, opts ) {
+  
+  var _$el;
+  var _$div1;
+  var _$div2;
+  var _render_round;   // compiled underscore template - nb: it's just a js function
+  var _api;
+
+  var _defaults = {
+                tplId: null
+              };
+  var _settings;
+  
+
+  function _debug( msg ) {
+      if( window.console && window.console.log ) {
+        window.console.log( '[debug] '+msg );
       }
     }
 
@@ -61,21 +100,31 @@ var sportdb_widget_new = function( widget_id, opts ) {
   
   
   
-  function _init( widget_id, opts )
+  function _init( id, opts )
   {
-    // todo: check if opts defined? - needed?
-    if( $.type( opts ) === 'undefined' )
-      opts = {};
+     if( typeof id === 'string' ) {
+       _debug( 'sportdb_widget_new id: ' + id );
+     }
+     else
+     {
+       _debug( 'sportdb_widget_new w/ el' );
+     }
+    
+     _settings = _.extend( {}, _defaults, opts );
 
-    // todo: use _.extend from underscore ???    
-    _settings = $.extend( _defaults, opts );
+     _debug( 'tplId: ' + _settings.tplId );
+     _debug( 'apiPathPrefix: ' + _settings.apiPathPrefix );
     
-     _debug( "tplId: " + _settings.tplId );
-     _debug( "apiPathPrefix: " + _settings.apiPathPrefix );
+     // pass along api opts if available
+     var api_opts = {};
+     if( _settings.apiPathPrefix !== undefined && _settings.apiPathPrefix !== null ) {
+       api_opts.apiPathPrefix = _settings.apiPathPrefix;
+     }
     
-     _api_path_prefix = _settings.apiPathPrefix;
+     _api = sportdb_api_new( api_opts );
+    
      
-     var tpl_str = "";
+     var tpl_str = '';
 
      if( _settings.tplId == null ) {
       // use builtin template
@@ -88,28 +137,60 @@ var sportdb_widget_new = function( widget_id, opts ) {
 
      _render_round  = _.template( tpl_str );
 
-    _$widget  = $( widget_id );
+    _$el  = $( id );
+    
+    _$div1 = $( '<div />' );
+    _$div2 = $( '<div />' );
+    _$el.append(  _$div1, _$div2 );
+    
+    _update( '1' );
+    _build_nav();
   }
 
   
-  function _fetch_round( event_key, round_pos, onsuccess )
-  {
-    var api_link = _api_path_prefix + "/event/" + event_key + "/round/" + round_pos + "?callback=?"; 
-    $.getJSON( api_link, onsuccess );
-  }
+  function _build_nav() {
 
-  function _update( event_key, round_pos )
+     _.each( _settings.rounds, function( round_pos, index ) {
+  
+       _debug( 'build round_pos ' + round_pos );
+       // _$div1.append( )
+       
+            var $link = $( "<a href='#'>" + round_pos + "</a>" );
+            $link.click( function() {
+               _debug( 'onclick - update round_pos: ' + round_pos );
+               _update( round_pos );
+             });
+            if( index > 0 ) {
+              _$div1.append( " | " );
+            }
+            _$div1.append( $link );
+     });
+
+/*
+    <p>
+   <a href="#" onclick="sportdb_widget.update( 'euro.2012', '1' ); return false;">1</a> |
+   <a href="#" onclick="sportdb_widget.update( 'euro.2012', '2' ); return false;">2</a> |
+   <a href="#" onclick="sportdb_widget.update( 'euro.2012', '3' ); return false;">3</a> |
+   <a href="#" onclick="sportdb_widget.update( 'euro.2012', '4' ); return false;">4</a> |
+   <a href="#" onclick="sportdb_widget.update( 'euro.2012', '5' ); return false;">5</a> |
+   <a href="#" onclick="sportdb_widget.update( 'euro.2012', '6' ); return false;">6</a>
+</p>
+*/
+  }
+  
+
+  function _update( round_pos )
   {
-    _fetch_round( event_key, round_pos, function( json ) {
+    _api.fetch_round( _settings.event, round_pos, function( json ) {
     
-        var snippet = _render_round( { data: json } );
-        _$widget.html( snippet );
+      var snippet = _render_round( { data: json } );
+      _$div2.html( snippet );
 
     }); 
   }  // fn _update
 
   // call "c'tor/constructor"
-  _init( widget_id, opts );
+  _init( id, opts );
 
   // return/export public api
   return {
@@ -123,43 +204,36 @@ var sportdb_widget_new = function( widget_id, opts ) {
 ////////////////////
 // wrapper for jquery plugin
 
-
-/*
-(function( $ ) {
+function register_jquery_fn_football( $ ) {
 
     function debug( msg ) {
       if( window.console && window.console.log ) {
-        window.console.log( "[debug] "+msg );
+        window.console.log( '[debug] '+msg );
       }
     }
 
+    debug( 'before add jquery fn football' );
+
     function setup_sportdb_widget( el, opts ) {
-      debug( "hello from setup_sportdb_widget" );
+      debug( 'hello from setup_sportdb_widget' );
       var sportdb_widget = sportdb_widget_new( el, opts );
       var $el = $(el);
       
-      // NB: attach table sorter to dom table element
-      // - use like $('#table1').data( 'table_sorter' ).hover().sort(1); etc.
-      //  or
-      //   var t = $('#table1').data( 'table_sorter' );
-      //    t.hover().sort(1);
       $el.data( 'widget', sportdb_widget );
-      
-      // fix: use opts event_key for setup!!!! or similar
-      sportdb_widget.update( 'euro.2012', '1' );
+
       return el;
     }
 
-    debug( 'add jquery fn football' );
-
     $.fn.football = function( opts ) {
-        debug( "calling football" );
+        debug( 'calling football' );
         return this.each( function( index, el ) {
-          debug( "before setup_sportdb_widget["+ index +"]" );
+          debug( 'before setup_sportdb_widget['+ index +']' );
           setup_sportdb_widget( el, opts );
-          debug( "after setup_sportdb_widget["+ index +"]" );
+          debug( 'after setup_sportdb_widget['+ index +']' );
         });
     };
 
-}( jQuery ));
-*/
+    debug( 'after add jquery fn football' );
+}
+
+register_jquery_fn_football( jQuery );
