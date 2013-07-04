1d4abaf4f353f00db5c04bf8368e7ca9127b2e75
Gerald Bauer <gerald.bauer@gmail.com>
2013-07-04 08:34:43 +0200

    use api module

--- js/jquery.football.widget.js
+++ js/jquery.football.widget.js
@@ -1,46 +1,5 @@
 
 
-////////////////////////////////////
-// football.dp api wrapper
-
-var sportdb_api_new = function( opts )
-{
-  var _defaults = {
-                apiPathPrefix: 'http://footballdb.herokuapp.com/api/v1',
-              };
-  var _settings;
-
-  function _debug( msg ) {
-      if( window.console && window.console.log ) {
-        window.console.log( '[debug] '+msg );
-      }
-    }
-
-  function _init( opts )
-  {
-    _settings = _.extend( {}, _defaults, opts );
-    
-    _debug( 'apiPathPrefix: ' + _settings.apiPathPrefix );
-  }
-
-  function _fetch_round( event_key, round_pos, onsuccess )
-  {
-    var api_url = _settings.apiPathPrefix + '/event/' + event_key + '/round/' + round_pos + '?callback=?'; 
-    $.getJSON( api_url, onsuccess );
-  }
-
-  // call "c'tor/constructor"
-  _init( opts );
-
-  // return/export public api
-  return {
-     fetch_round: _fetch_round
-  }
-} // fn sportdb_api_new
-
-
-
-
 var sportdb_widget_new = function( id, opts ) {
   
   var _$el;
